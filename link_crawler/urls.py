31fa08485fc8863498b0425f31419791cfd33655
samiulhuda360 <samidurbar@gmail.com>
2024-02-26 00:42:37 +0600

    staticfiles

--- link_crawler/urls.py
+++ link_crawler/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import LogoutView
+
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -11,4 +13,5 @@ urlpatterns = [
     path('add-links', views.add_links, name='add_links'),
     path('api/', include(router.urls)),
     path('download-excel-template/', views.download_excel_template, name='download_excel_template'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
