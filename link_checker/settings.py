a6ddafbfdb3ae22b0e12e86b827d031e42d6dc5a
samiulhuda360 <samidurbar@gmail.com>
2024-02-26 02:50:58 +0600

    Celery Beat Setup

--- link_checker/settings.py
+++ link_checker/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'rest_framework',
     'rest_framework.authtoken',
     'link_crawler',
