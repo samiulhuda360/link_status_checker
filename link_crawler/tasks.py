2cb4c0e1131bc71556aa687f60db932040249616
samiulhuda360 <samidurbar@gmail.com>
2024-02-27 18:31:57 +0600

    Disable Debugging

--- link_crawler/tasks.py
+++ link_crawler/tasks.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from django.db.models import Q
 from .models import Link  
 from celery.utils.log import get_task_logger
+from celery import group
+
 
 
 logger = get_task_logger(__name__)
@@ -61,16 +63,22 @@ def inspect_links(target_url, link_to, anchor_text):
         return 'Error', datetime.now().date()
 
 @shared_task
+def crawl_single_link(link_id):
+    link = Link.objects.get(id=link_id)
+    if not link.manual_edit:
+        status, last_crawl = inspect_links(link.target_link, link.link_to, link.anchor_text)
+        logger.info(f"Updating link {link.link_to} status to {status}")
+        link.status_of_link = status
+        link.last_crawl_date = last_crawl
+        link.save()
+
+@shared_task
 def crawl_and_update_links():
     logger.info("Starting crawl_and_update_links task")
-    links_to_check = Link.objects.all()  # Adjust based on your criteria
-
-    for link in links_to_check:
-        if not link.manual_edit:
-            status, last_crawl = inspect_links(link.target_link, link.link_to, link.anchor_text)
-            logger.info(f"Updating link {link.link_to} status to {status}")
-            link.status_of_link = status
-            link.last_crawl_date = last_crawl
-            link.save()
-
-    logger.info("Finished crawl_and_update_links task")
+    links_to_check = Link.objects.all().values_list('id', flat=True)
+    
+    # Create a group of subtasks to process each link independently
+    job = group(crawl_single_link.s(link_id) for link_id in links_to_check)
+    result = job.apply_async()
+    
+    logger.info("Finished scheduling crawl_and_update_links task")
